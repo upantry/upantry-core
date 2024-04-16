a8078a6f9647244c897002059e898b28ab772047
Jacob Ledgerwood <jacob.g.ledgerwood@gmail.com>
2024-04-16 14:35:42 -0400

    Mock endpoint (#5)

--- src/main/kotlin/com/upantry7/upantrycore/http/model/HttpMessages.kt
+++ src/main/kotlin/com/upantry7/upantrycore/http/model/HttpMessages.kt
@@ -1,3 +1,10 @@
 package com.upantry7.upantrycore.http.model
 
 data class HelloResponse(val response: String = "Hello world!")
+
+
+data class TranscribeIngredientsRequest(val image: ByteArray)
+data class TranscribeIngredientsResponse(val ingredients: String)
+
+data class GenerateRecipeRequest(val ingredients: String, val choice: String)
+data class GenerateRecipeResponse(val ingredients: String)
