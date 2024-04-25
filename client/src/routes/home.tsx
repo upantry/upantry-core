f2e1e39672fb3ccbf631df29da2f7bce0e914e13
Rémi Vansteelandt <remvst@gmail.com>
2024-04-25 15:13:12 -0400

    FE polish

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -15,6 +15,7 @@ function CarouselSlide(props: { image: string }) {
         background: "black",
         backgroundImage: `url(${props.image})`,
         backgroundSize: "cover",
+        filter: 'brightness(70%)'
       }}
     ></div>
   );
@@ -42,31 +43,40 @@ export function Home() {
       <Content>
         <Carousel>
           <Carousel.Item>
-            <CarouselSlide image="/slide1.jpg" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
             <CarouselSlide image="/slide2.jpg" />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <CarouselSlide image="/slide3.jpg" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Cook with AI</h3>
+              <p>Cook more, spend less and save planet</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <CarouselSlide image="/slide4.jpg" />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Small Kitchen, Big Impact</h3>
+
+<div
+  className="counter-container"
+  style={{
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: "1rem",
+  }}
+>
+  <AnimatedCounter
+    value={count}
+    includeDecimals={false}
+    color="white"
+    fontSize="40px"
+  />
+  Recipes Generated
+</div>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <CarouselSlide image="/slide3.jpg" />
+            <Carousel.Caption>
+              <h3>Our Mission</h3>
+              <p>109 billion lbs are wasted every year in the US. Help us fight it!</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
@@ -75,23 +85,19 @@ export function Home() {
           Get Started
         </Button>
 
-        <div
-          className="counter-container"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            paddingTop: "1rem",
-          }}
-        >
-          <AnimatedCounter
-            value={count}
-            includeDecimals={false}
-            color="black"
-            fontSize="40px"
-          />
-        </div>
+        <h2>Cook More, Spend Less</h2>
+        <p>We believe <strong>big changes start with small habits</strong>. Bring your own bags, ditch disposables, and fight food waste!</p>
 
-        <h2>Recently enjoyed plates</h2>
+
+        <h2>Did you know?</h2>
+        <p>In America, <strong>nearly 40% of the food supply is wasted every year</strong>. That translates to hundreds of dollars worth of groceries wasted per person annually.</p>
+        <p>This has major environmental consequences. Food waste takes up a huge portion of landfill space, releasing harmful greenhouse gasses as it decomposes.  In fact, <strong>it's the single largest component of municipal solid waste in the country</strong>.</p>
+
+        <h2>Mission</h2>
+        <p>Our goal is to optimize your pantry by crafting delicious and sustainable meals from existing groceries.</p>
+        <p>This approach emphasizes maximizing the value of your current ingredients while minimizing waste and environmental impact.</p>
+
+        <p style={{textAlign: 'center', fontSize: '0.8em', marginTop: '50px'}}>Made with ♥ in Cambridge, ON</p>
       </Content>
 
       <ActionBar>
